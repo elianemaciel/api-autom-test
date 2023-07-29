b2f860107e287e1714c19aa1d6243645a60f5f53
joanderson.goncalves <joanderson.goncalves@ufba.br>
2023-07-29 18:12:58 -0300

    feat: Add Specify Equiv Classes starting page

--- assets/ui/util/style.py
+++ assets/ui/util/style.py
@@ -1,1 +1,2 @@
 BASIC_APPLICATION_TEXT = 'color: black; font-family: Arial; font-size: 18px; text-align: center;'
+EXPLANATION_APPLICATION_TEXT = 'color: black; font-family: Arial; font-size: 16px; text-align: fill;'
--- assets/ui/windows/specify_equiv_classes_page.py
+++ assets/ui/windows/specify_equiv_classes_page.py
@@ -1,9 +1,10 @@
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QPixmap
-from PySide6.QtWidgets import QSpacerItem, QSizePolicy, QWidget, QVBoxLayout, QLabel
+from PySide6.QtWidgets import QSpacerItem, QSizePolicy, QWidget, QVBoxLayout, QLabel, QHBoxLayout
 import os
 
-from assets.ui.util import style
+from assets.ui.util import style, color
+from assets.ui.widgets.menu_button import AtMenuButton
 
 
 class SpecifyEquivClassesWidget:
@@ -13,7 +14,8 @@ class SpecifyEquivClassesWidget:
     @staticmethod
     def get_or_start(position=None):
         if SpecifyEquivClassesWidget.position is None and position is None:
-            raise ValueError("InsertMethodsInfo not started yet. position has value 'None'. Please start the page first")
+            raise ValueError(
+                "InsertMethodsInfo not started yet. position has value 'None'. Please start the page first")
         if SpecifyEquivClassesWidget.position is not None:
             return SpecifyEquivClassesWidget.instance
         SpecifyEquivClassesWidget.position = position
@@ -26,32 +28,93 @@ class SpecifyEquivClassesWidget:
         content_layout = QVBoxLayout()
 
         # Set spacing
-        spacing = QSpacerItem(20, 20, QSizePolicy.Minimum, QSizePolicy.Expanding)
+        spacing = QSpacerItem(20, 20, QSizePolicy.Fixed, QSizePolicy.Fixed)
         content_layout.addItem(spacing)
 
-        # Set application logo
-        logo = QLabel()
-        pixmap = QPixmap(os.path.join(os.getcwd(), 'ui/images/automtest-logo.png'))
-        scaled_pixmap = pixmap.scaledToHeight(64)
-        logo.setPixmap(scaled_pixmap)
-        logo.resize(scaled_pixmap.width(), scaled_pixmap.height())
-        logo.setAlignment(Qt.AlignCenter)
-        content_layout.addWidget(logo)
+        # # Set application logo
+        # logo = QLabel()
+        # pixmap = QPixmap(os.path.join(os.getcwd(), 'ui/images/automtest-logo.png'))
+        # scaled_pixmap = pixmap.scaledToHeight(64)
+        # logo.setPixmap(scaled_pixmap)
+        # logo.resize(scaled_pixmap.width(), scaled_pixmap.height())
+        # logo.setAlignment(Qt.AlignCenter)
+        # content_layout.addWidget(logo)
 
         # Set spacing
-        spacing = QSpacerItem(20, 20, QSizePolicy.Minimum, QSizePolicy.Expanding)
-        content_layout.addItem(spacing)
+        # spacing = QSpacerItem(20, 20, QSizePolicy.Minimum, QSizePolicy.Expanding)
+        # content_layout.addItem(spacing)
 
         # Set main content
-        program_description = QLabel()
-        program_description.setText("Content for Specify Equiv Classes Info page")
-        program_description.setStyleSheet(style.BASIC_APPLICATION_TEXT)
-        program_description.setWordWrap(True)
-        content_layout.addWidget(program_description)
+        title = QLabel()
+        title.setText("Equivalence Classes")
+        title.setStyleSheet(style.BASIC_APPLICATION_TEXT)
+        title.setAlignment(Qt.AlignCenter)
+        title.setWordWrap(True)
+        content_layout.addWidget(title)
+
+        # Set spacing
+        spacing = QSpacerItem(20, 20, QSizePolicy.Fixed, QSizePolicy.Fixed)
+        content_layout.addItem(spacing)
+
+        # what = QLabel()
+        # what.setText("""
+        #
+        # """)
+        # what.setStyleSheet(style.EXPLANATION_APPLICATION_TEXT)
+        # what.setWordWrap(True)
+        # content_layout.addWidget(what)
+
+        an_example = QLabel()
+        an_example.setText("""
+            What Are:
+            Here, an equivalence class is a tuple of a set of methods parameters and a set of possible return values for those parameters.
+                
+            Observe the following example:
+            the method isMaiorDeIdade(inteiro idade), which has as responsibility check whether a person's of legal age, returns true in case the provided age is greater tha or equal to 18 years old and false otherwise. For that example,  we could define two equivalence classes: 
+            1. The class of legal age people, with return value true and, in the parameters set 18 and 65.
+            2. The class of minor agr people, with return value false and, as parameter set, 0, 1, 15 and 17, for
+            example.
+                
+            With those info, it's possible to define a equivalence class.
+            Now it's your your turn: insert the equivalence classes you decide relevant to each previously mapped
+            method.
+        """)
+        an_example.setStyleSheet(style.EXPLANATION_APPLICATION_TEXT)
+        an_example.setWordWrap(True)
+        content_layout.addWidget(an_example)
 
         # Set spacing
         spacing = QSpacerItem(20, 20, QSizePolicy.Minimum, QSizePolicy.Expanding)
         content_layout.addItem(spacing)
+
+        # bottom buttons
+        bottom_layout = QHBoxLayout()
+        bottom_layout.addWidget(
+            AtMenuButton(
+                text="List equivalence\n classes",
+                # height=30,
+                maximum_width=170,
+                minimum_width=170,
+                do_when_clicked=lambda: print("Voltando para onde estávamos antes"),
+                btn_color=color.BOTTOM_NAVIGATION_LIST_ALL
+            )
+        )
+        # Set spacing
+        bottom_layout.addItem(QSpacerItem(20, 20, QSizePolicy.Expanding, QSizePolicy.Minimum))
+        bottom_layout.addWidget(
+            AtMenuButton(
+                text="Add",
+                minimum_width=220,
+                do_when_clicked=show_next_content(),
+                btn_color=color.BOTTOM_NAVIGATION_FORWARD
+            )
+        )
+        content_layout.addLayout(bottom_layout)
 
         about_page.setLayout(content_layout)
         return about_page
+
+
+def show_next_content():
+    # TODO: implement
+    pass
