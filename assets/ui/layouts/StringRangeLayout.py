07fca1b5a5aa21ebe0a5451e6bc5a331fb8f1d81
JoandersonG <joandersongsantos@yahoo.com>
2023-09-08 16:16:53 -0300

    feat: Add Move and Remove in String Range && fix get_range_data not returning all range

--- assets/ui/layouts/StringRangeLayout.py
+++ assets/ui/layouts/StringRangeLayout.py
@@ -23,16 +23,16 @@ class StringRangeLayout(QVBoxLayout):
         self.addLayout(param_def_layout)
 
         horizontal_scroll = QScrollArea()
-        horizontal_scroll.setFixedHeight(137)
+        horizontal_scroll.setFixedHeight(170)
         horizontal_scroll.setWidgetResizable(True)
         horizontal_scroll_content = QWidget(horizontal_scroll)
-        horizontal_scroll_content.setFixedHeight(120)
+        horizontal_scroll_content.setFixedHeight(153)
         self.horizontal_scroll_layout = QHBoxLayout(horizontal_scroll_content)
         horizontal_scroll.setWidget(horizontal_scroll_content)
         horizontal_scroll.setStyleSheet("border: none;")
 
         param_widget = QWidget()
-        param_widget.setFixedHeight(100)
+        param_widget.setFixedHeight(133)
         param_widget.setStyleSheet("border-radius: 10px; background-color: " + color.LIGHT_GRAY + ";")
 
         param_label_layout = QVBoxLayout()
@@ -46,6 +46,10 @@ class StringRangeLayout(QVBoxLayout):
         param_label_layout.addWidget(label)
 
         label = QLabel("Quantity:")
+        label.setStyleSheet("font-family: Arial; font-size: 14px;")
+        param_label_layout.addWidget(label)
+
+        label = QLabel("Move or Remove:")
         label.setStyleSheet("font-family: Arial; font-size: 14px;")
         param_label_layout.addWidget(label)
 
@@ -63,18 +67,16 @@ class StringRangeLayout(QVBoxLayout):
             curr_range_start = curr_range.split("~")[0]
             curr_range_end = curr_range.split("~")[1]
 
-            self.horizontal_scroll_layout.addWidget(
-                build_param_range_element(curr_range_end, curr_range_start, curr_substr))
+            element = self.build_param_range_element(curr_range_end, curr_range_start, curr_substr)
+            self.horizontal_scroll_layout.addWidget(element)
 
         self.horizontal_scroll_layout.addItem(QSpacerItem(20, 20, QSizePolicy.Expanding, QSizePolicy.Minimum))
 
         param_label_wrapper_layout = QVBoxLayout()
         param_label_wrapper_layout.addItem(QSpacerItem(10, 10, QSizePolicy.Fixed, QSizePolicy.Fixed))
         param_label_wrapper_layout.addWidget(AtMenuButton(
-            # id=self.horizontal_scroll_layouts.index(self.horizontal_scroll_layout),
-            # id=position_in_parent,
             text="Add\nmore",
-            height=100,
+            height=133,
             minimum_width=70,
             font_size=10,
             btn_color=color.ADD_NEW_METHOD_BUTTON,
@@ -87,84 +89,138 @@ class StringRangeLayout(QVBoxLayout):
         return self.horizontal_scroll_layout
 
     def add_more_on_click(self):
-        return self.horizontal_scroll_layout.addWidget(build_param_range_element("", "", ""))
+        return self.horizontal_scroll_layout.addWidget(self.build_param_range_element("", "", ""))
 
     def get_range_data(self):
         range_content_builder = ""
         range_quantity_builder = ""
         try:
-            for j in range(0, self.count()):
-                if isinstance(self.horizontal_scroll_layout.itemAt(j), QSpacerItem) \
-                        or isinstance(self.horizontal_scroll_layout.itemAt(j).widget(), QLabel):
+            for j in range(0, self.horizontal_scroll_layout.count()):
+                item = self.horizontal_scroll_layout.itemAt(j)
+                if isinstance(item, QSpacerItem) or isinstance(item.widget(), QLabel):
                     continue
-                type_combo_selected = self.horizontal_scroll_layout.itemAt(j).widget().layout().itemAt(0).itemAt(0).widget().currentText()
-                content_text = self.horizontal_scroll_layout.itemAt(j).widget().layout().itemAt(1).widget().text()
-                start_value = self.horizontal_scroll_layout.itemAt(j).widget().layout().itemAt(2).layout().itemAt(0).widget().text()
-                end_value = self.horizontal_scroll_layout.itemAt(j).widget().layout().itemAt(2).layout().itemAt(2).widget().text()
-                range_content_builder += "[" + content_text + "]"
+                type_combo_selected = item.widget().layout().itemAt(0).itemAt(0).widget().currentText()
+                content_text = item.widget().layout().itemAt(1).widget().text()
+                start_value = item.widget().layout().itemAt(2).layout().itemAt(0).widget().text()
+                end_value = item.widget().layout().itemAt(2).layout().itemAt(2).widget().text()
+
+                if type_combo_selected == "any":
+                    range_content_builder += "[" + content_text + "]"
+                else:
+                    range_content_builder += "[" + type_combo_selected + "]"
                 range_quantity_builder += "[" + start_value + "~" + end_value + "]"
         except Exception as e:
             print("An internal error occurred when getting data range from StringRangeLayout", e)
+        print("get_range_data: " + range_content_builder + " - " + range_quantity_builder)
         return range_content_builder, range_quantity_builder
+
+    def build_param_range_element(self, curr_range_end, curr_range_start, curr_substr):
+        param_widget = QWidget()
+        param_widget.setFixedHeight(133)
+        param_widget.setStyleSheet("border-radius: 10px; background-color: " + color.LIGHT_GRAY + ";")
+        param_layout = QVBoxLayout()
+
+        content_text_edit = QLineEdit()
+
+        # type
+        type_layout = QHBoxLayout()
+        type_combo_box = CustomComboBox(do_after_set_index=lambda i: (
+                content_text_edit.setEnabled(i == 0),
+                content_text_edit.setStyleSheet(get_line_edit_stylesheet("gray")))
+            )
+        type_combo_box.setObjectName("type_combo_box")
+        type_combo_box.addItem("any")
+        type_combo_box.addItem("sign")
+        type_combo_box.addItem("number")
+        type_combo_box.addItem("letter")
+        type_combo_box.addItem("alphanumeric")
+        type_layout.addWidget(type_combo_box)
+        type_layout.addWidget(AtMenuButton(
+            height=20,
+            minimum_width=30,
+            maximum_width=30,
+            font_size=10,
+            border_radius=5,
+            text="?",
+            btn_color=color.HELP_BUTTON,
+            do_when_clicked=lambda: remove_and_update_view(param_widget)
+        ))
+        param_layout.addLayout(type_layout)
+        # content
+        # for type: ANY
+        content_text_edit.setObjectName("content_text_edit")
+        content_text_edit.setStyleSheet(get_line_edit_stylesheet("white"))
+        content_text_edit.setText(curr_substr)
+        content_text_edit.setAlignment(Qt.AlignCenter)
+        param_layout.addWidget(content_text_edit)
+        # quantity
+        quantity_layout = QHBoxLayout()
+        start_quantity_text_edit = QLineEdit()
+        start_quantity_text_edit.setObjectName("start_quantity_text_edit")
+        start_quantity_text_edit.setStyleSheet(get_line_edit_stylesheet("white"))
+        start_quantity_text_edit.setAlignment(Qt.AlignCenter)
+        start_quantity_text_edit.setText(curr_range_start)
+        quantity_layout.addWidget(start_quantity_text_edit)
+        label = QLabel("to")
+        label.setStyleSheet("padding:2px; font-family: Arial; font-size: 14px;")
+        quantity_layout.addWidget(label)
+        end_quantity_text_edit = QLineEdit()
+        end_quantity_text_edit.setObjectName("end_quantity_text_edit")
+        end_quantity_text_edit.setStyleSheet(get_line_edit_stylesheet("white"))
+        end_quantity_text_edit.setAlignment(Qt.AlignCenter)
+        end_quantity_text_edit.setText(curr_range_end)
+        quantity_layout.addWidget(end_quantity_text_edit)
+        param_layout.addLayout(quantity_layout)
+
+        bottom_items_layout = QHBoxLayout()
+        param_layout.addLayout(bottom_items_layout)
+
+        bottom_items_layout.addWidget(AtMenuButton(
+            height=30,
+            minimum_width=30,
+            maximum_width=30,
+            font_size=10,
+            border_radius=5,
+            text="<",
+            btn_color=color.EDIT_BUTTON,
+            do_when_clicked=lambda: self.move_widget(param_widget, "left")
+        ))
+        bottom_items_layout.addWidget(AtMenuButton(
+            height=30,
+            minimum_width=70,
+            # maximum_width=30,
+            font_size=10,
+            border_radius=5,
+            text="Remover",
+            btn_color=color.REMOVE_BUTTON,
+            do_when_clicked=lambda: remove_and_update_view(param_widget)
+        ))
+        bottom_items_layout.addWidget(AtMenuButton(
+            height=30,
+            minimum_width=30,
+            maximum_width=30,
+            font_size=10,
+            border_radius=5,
+            text=">",
+            btn_color=color.EDIT_BUTTON,
+            do_when_clicked=lambda: self.move_widget(param_widget, "right")
+        ))
+
+        param_widget.setLayout(param_layout)
+        return param_widget
+
+    def move_widget(self, param_widget, direction):
+        index = self.horizontal_scroll_layout.indexOf(param_widget)
+        if direction == "left" and index - 1 >= 0:
+            index -= 1
+        if direction == "right" and index + 1 < (self.horizontal_scroll_layout.count() - 1):
+            index += 1
+        self.horizontal_scroll_layout.insertWidget(index, param_widget)
 
 
 def remove_and_update_view(widget_to_remove):
     widget_to_remove.deleteLater()
 
 
-def build_param_range_element(curr_range_end, curr_range_start, curr_substr):
-    param_widget = QWidget()
-    param_widget.setFixedHeight(100)
-    param_widget.setStyleSheet("border-radius: 10px; background-color: " + color.LIGHT_GRAY + ";")
-    param_layout = QVBoxLayout()
-    # param_layout.setAlignment(Qt.AlignCenter)
-    # type
-    type_layout = QHBoxLayout()
-    type_combo_box = CustomComboBox()
-    type_combo_box.setObjectName("type_combo_box")
-    type_combo_box.addItem("any")
-    type_combo_box.addItem("sign")
-    type_combo_box.addItem("number")
-    type_combo_box.addItem("letter")
-    type_combo_box.addItem("alphanumeric")
-    type_layout.addWidget(type_combo_box)
-    type_layout.addWidget(AtMenuButton(
-        height=20,
-        minimum_width=30,
-        maximum_width=30,
-        font_size=10,
-        border_radius=5,
-        text="X",
-        btn_color=color.LIGHT_REMOVE_BUTTON,
-        do_when_clicked=lambda: remove_and_update_view(param_widget)
-    ))
-    param_layout.addLayout(type_layout)
-    # content
-    # for type: ANY
-    content_text_edit = QLineEdit()
-    content_text_edit.setObjectName("content_text_edit")
-    styleSheet = "border-radius: 5px; background-color: white; padding: 5px;font-family: Arial; font-size: 14px;"
-    content_text_edit.setStyleSheet(styleSheet)
-    content_text_edit.setText(curr_substr)
-    content_text_edit.setAlignment(Qt.AlignCenter)
-    param_layout.addWidget(content_text_edit)
-    # quantity
-    quantity_layout = QHBoxLayout()
-    start_quantity_text_edit = QLineEdit()
-    start_quantity_text_edit.setObjectName("start_quantity_text_edit")
-    start_quantity_text_edit.setStyleSheet(styleSheet)
-    start_quantity_text_edit.setAlignment(Qt.AlignCenter)
-    start_quantity_text_edit.setText(curr_range_start)
-    quantity_layout.addWidget(start_quantity_text_edit)
-    label = QLabel("to")
-    label.setStyleSheet("padding:2px; font-family: Arial; font-size: 14px;")
-    quantity_layout.addWidget(label)
-    end_quantity_text_edit = QLineEdit()
-    end_quantity_text_edit.setObjectName("end_quantity_text_edit")
-    end_quantity_text_edit.setStyleSheet(styleSheet)
-    end_quantity_text_edit.setAlignment(Qt.AlignCenter)
-    end_quantity_text_edit.setText(curr_range_end)
-    quantity_layout.addWidget(end_quantity_text_edit)
-    param_layout.addLayout(quantity_layout)
-    param_widget.setLayout(param_layout)
-    return param_widget
+def get_line_edit_stylesheet(color):
+    return "border-radius: 5px; background-color: " + color + "; padding: 5px;font-family: Arial; font-size: 14px;"
