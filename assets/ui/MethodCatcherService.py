21db5e37565709f9322757efa549671ba94282b7
JoandersonG <joandersongsantos@yahoo.com>
2024-08-04 23:49:31 -0300

    style: clear MethodCatcherService print and deprecated logic

--- assets/ui/MethodCatcherService.py
+++ assets/ui/MethodCatcherService.py
@@ -17,32 +17,20 @@ def get(user_story_txt, language):
 
 def _run_llm():
     while True:
-        print('entrou no while')
         with while_lock:
             _update_methods_result_and_percentage()
-            print('entrou no while_lock')
             if method_catcher_repository.get_current_state_percentage() >= 100:
                 break
-            try:
-                print("running pending")
-                # schedule.run_pending()
-            except Exception as e:
-                traceback.print_exc()
-                print("Error while getting extra methods suggestions from user story: " + e.__str__())
             time.sleep(1)
-    # schedule.clear()
     print('Exited _run_llm')
 
 
 def _update_methods_result_and_percentage():
     with lock:
-        print("_update_methods_result_and_percentage start")
         # LLMRepository.get_extra_suggestions_from_user_story()
         method_catcher_repository.compute_extra_methods()
         curr_percentage = method_catcher_repository.get_current_state_percentage()
         # global progress_dialog
         if curr_percentage >= 100:
             # schedule.clear()
-            print("should stop method")
             return
-        print("Running method. Percentage: " + str(curr_percentage))
