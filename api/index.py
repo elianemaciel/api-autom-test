e5bf055af6fdab907e7c4892b998caed1bd89ea3
Eliane Maciel <eliane.faveron@gmail.com>
2026-08-23 20:30:23 -0300

    teste

--- api/index.py
+++ api/index.py
@@ -1,5 +1,21 @@
+from urllib.parse import parse_qs
+
 from run import app
 
 
 # Vercel's Python runtime looks for a WSGI-compatible variable.
-handler = app
+class VercelPathMiddleware:
+    def __init__(self, wrapped_app):
+        self.wrapped_app = wrapped_app
+
+    def __call__(self, environ, start_response):
+        query_params = parse_qs(environ.get('QUERY_STRING', ''))
+        rewritten_path = query_params.get('path', [None])[0]
+
+        if rewritten_path:
+            environ['PATH_INFO'] = rewritten_path
+
+        return self.wrapped_app(environ, start_response)
+
+
+handler = VercelPathMiddleware(app)
